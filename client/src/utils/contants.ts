6396eae59efc4f45cd3c76fce3b91a04d2cf929b
Andrey Kruglik <isekaiguy@protonmail.com>
2022-07-24 20:23:08 +0700

    Smart contract finished

---
+++ client/src/utils/contants.ts
@@ -0,0 +1,4 @@
+import abi from './Transactions.json';
+
+export const contractABI = abi.abi;
+export const contractAddress = '0x3A0eeDB921f14E0D2640Ba72B350a0CFbCEEBB1E';
--- smart-contract/hardhat.config.js
+++ smart-contract/hardhat.config.js
@@ -1,6 +1,13 @@
-require("@nomicfoundation/hardhat-toolbox");
+//https://eth-goerli.g.alchemy.com/v2/teAV6dVlKpTgkLuIbZbgTvTehl4M9m9P
+require('dotenv').config();
+require('@nomiclabs/hardhat-waffle');
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.9",
-};
+    solidity: '0.8.0',
+    networks: {
+        goerli: {
+            url: 'https://eth-goerli.g.alchemy.com/v2/teAV6dVlKpTgkLuIbZbgTvTehl4M9m9P',
+            accounts: [`${process.env.ACCOUNT_NAME}`]
+        }
+    }
+}
