53f4a0e57c7af325adc6751a75ec716dc11beaa0
Andrey Kruglik <isekaiguy@protonmail.com>
2022-07-24 11:33:05 +0700

    Added main styles

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,19 @@
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Welcome from "./components/Welcome";
+import Services from "./components/Services";
+import Transactions from "./components/Transactions";
+
 const App = () => {
-  return (<div className='text-3xl'>Hello</div>)
+  return (<div className='min-h-screen'>
+    <div className='gradient-bg-welcome'>
+      <Navbar/>
+      <Welcome/>
+    </div>
+    <Services/>
+    <Transactions/>
+    <Footer/>
+  </div>)
 }
 
 export default App;
